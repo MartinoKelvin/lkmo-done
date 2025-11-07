94f6bd6af4b75d8b06eb7ff2030fb844270d2bfb
MartinoKelvin <martinokelvin06032005@gmail.com>
2025-11-07 23:01:27 +0700

    url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if($this->app->environment('production')) {
             // Force HTTPS in production
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 }
